ac1d8349c01bfc9aac12851b7b5fe3c5303bd3fb
Kovman98 <kovman@KovisWorld>
2023-10-04 09:55:51 +0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Playing!!
+          I was changed!!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on feature
+          Learn React
         </a>
       </header>
     </div>
